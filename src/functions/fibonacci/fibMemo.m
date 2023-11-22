62b946d792b9051c1acfa081aba13331a37aa952
Vasilis Aitsidis <bilonio02@gmail.com>
2023-11-22 16:15:11 +0200

    Remove unnecessary files

--- src/functions/fibonacci/fibMemo.m
+++
@@ -1,9 +0,0 @@
-function [fibn] = fibMemo(n)
-fs = [1,1]; %0-th Fibonacci is one
-
-for i = 1:n-2
-    fs = [fs, fs(end) + fs(end-1)];
-end
-
-fibn = fs(end);
-end
--- src/functions/fibonacci/inverseFib.m
+++
@@ -1,6 +0,0 @@
-function [n] = inverseFib(fibn)
-phi = (1 + sqrt(5)) / 2;
-
-n = floor(log(fibn * sqrt(5) + 1/2) / log(phi));
-
-end
