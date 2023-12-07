ccba4ba228ef09a52202ec5f8be6b28da6e0b44c
Vasilis Aitsidis <bilonio02@gmail.com>
2023-12-08 00:04:54 +0200

    Commit ex1b.m

---
+++ Work3/src/ex1/ex1b.m
@@ -0,0 +1,85 @@
+clc;
+clear;
+close all;
+
+syms x1 x2;
+
+
+% create directories
+formats = ["eps", "png", "jpg"];
+
+for i = 1:length(formats)
+    mkdir(fullfile('../../figures', 'ex1', sprintf('%s', formats(i))));
+end
+
+
+% init params
+f=(1/3)*(x1^2)+3*(x2^2);
+
+f1 = inline(f);
+fx = @(x) f1(x(:,1), x(:,2));
+
+
+ 
+x0 = [8 -10];
+celoptionarray = [0.1,0.3,3,5];
+titleoptionarray = [0.1, 0.3,3,5]; 
+subPlotPositions = {1, 2, 3, 4};
+
+count = 1;
+
+    X0 = sprintf('For initial point (%d, %d)\n', x0(1,1), x0(1,2));
+    disp(X0);
+    fig = figure();
+    fig.WindowState = 'maximized';
+    for j = 1:length(celoptionarray)
+
+        [res] = steepestDescent(1e-3, x0, f, celoptionarray(j));
+
+        results(count).xmin = res.xmin;
+        results(count).dist = res.dist;
+        results(count).gammas = res.gammas;
+        results(count).time = res.time;
+        results(count).xs = res.xs;
+        results(count).k = res.k;
+        results(count).method = res.method;
+        results(count).x0 = x0;
+
+        subplot(2,2, subPlotPositions{j});
+        plot(1:res.k, fx(res.xs), '-ob');
+        
+        hold on;
+        yline(fx(res.xmin), '-.r');
+        
+
+        xlabel('k Αριθμός επαναλήψεων', 'Interpreter','tex')
+        ylabel('Τιμή της f(x_k)', 'Interpreter','tex');
+        legend('Τιμή της f', 'Ελάχιστη τιμή της f');
+        title(sprintf('Τιμή της f με γκ=  %d για αρχικό σημείο (x, y) = (%d, %d) ~ k αριθμός επαναλήψεων', titleoptionarray(j), x0(1,1), x0(1,2)), 'Interpreter', 'tex');
+
+        count = count+1;
+
+    end
+
+    for k  = 1:length(formats)
+         % delete and save new plots
+         delete(fullfile('../../figures', 'ex1', sprintf("%s", formats(k)), sprintf("ex1b(%.0f, %.0f).%s", x0(1,1), x0(1,2), formats(k))));
+         saveas(fig, fullfile('../../figures', 'ex1', sprintf("%s", formats(k)), sprintf("ex1b(%.0f, %.0f).%s", x0(1,1), x0(1,2), formats(k))));
+    end
+
+
+% find minimums and keep index
+[minDist, indexDist] = min([results.dist]);
+[minIterations, indexIterations] = min([results.k]);
+[minTime, indexTime] = min([results.time]);
+[minXmin, indexXmin] = min([results.xmin]);
+
+% display minimums
+dist = ['x0 =' , num2str(results(indexDist).x0), ', method ', results(indexDist).method, 'for minimum distance of ', num2str(minDist)];
+disp(dist);
+iterations = ['x0 = ', num2str(results(indexIterations).x0), ', method ', results(indexIterations).method, 'for minimum iterations of ', num2str(minIterations)];
+disp(iterations);
+time = ['x0 = ', num2str(results(indexTime).x0), ', method ', results(indexTime).method, 'for minimum running time of ', num2str(minTime)];
+disp(time);
+xmin = ['x0 = ', num2str(results(indexXmin).x0), ', method ', results(indexXmin).method, 'for minimum final X of ', num2str(minXmin)];
+disp(xmin);
